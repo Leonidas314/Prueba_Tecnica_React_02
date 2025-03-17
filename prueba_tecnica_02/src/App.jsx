0181ac095c227e07ffc36e7ba6dd128b72bb6c8f
Leon <leondellafiore2016@gmail.com>
2025-03-17 18:15:47 -0300

    Add h1, form with button and input, main with h2 and basic centering css to App

--- prueba_tecnica_02/src/App.jsx
+++ prueba_tecnica_02/src/App.jsx
@@ -4,7 +4,18 @@ import './App.css'
 function App() {
 
   return (
-      
+    <div className='page'>
+      <header>
+          <h1>Prueba Tecnica: Buscador de Peliculas</h1>
+          <form className='form' action="">
+            <button type='submit'>Buscar</button>   
+            <input type="Peliculas" placeholder='Shrek, Batman, Spiderman' />
+          </form>
+      </header>
+      <main>
+        <h2>Resultados de la busqueda</h2>
+      </main>
+    </div>
   )
 }
 
